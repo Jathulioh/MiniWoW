978c60aa13c7bdf392c91f76d7e5232ff40f06a6
Jathulioh <JDWhittall@gmail.com>
2019-12-10 02:57:26 +0000

    Added Draenei Started work on character customization

---
+++ Assets/CharacterCustomization.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public struct appearances{
+
+	public enum Appearances {SKIN, FACE, HAIR_STYLE, HAIR_COLOR, FACIAL_HAIR, PIERCINGS, EARINGS, EARS, ACCESSORIES, HORNS, MARKINGS, TATTOO, TATTOO_COLOR};
+	public Appearances detail;
+	public Texture2D[] texturePart;
+	public GameObject[] affectedLayer;
+	public Material[] affectedPart;
+
+	}
+
+public class CharacterCustomization : MonoBehaviour
+{
+
+	public appearances[] appearance;
+
+}
---
+++ Assets/HumanMaleCustomization.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HumanMaleCustomization : CharacterCustomization
+{
+	public int skinColorIndex;
+
+	private void Update()
+	{
+		if (Input.GetKeyUp(KeyCode.RightBracket)){
+			skinNext();
+		}
+		if (Input.GetKeyUp(KeyCode.LeftBracket)){
+			skinPrevious();
+		}
+	}
+	public void skinNext()
+	{
+		skinColorIndex++;
+	}
+	public void skinPrevious()
+	{
+		skinColorIndex--;
+	}
+	public void GetLayer(appearances.Appearances detail)
+	{
+		for (int i = 0; i < appearance.Length; i++)
+		{
+			if(this.gameObject.GetComponent<CharacterCustomization>().appearance[i].detail == appearances.Appearances.SKIN) {
+				Debug.Log("This is el skin");
+			}
+		}
+	}
+}
